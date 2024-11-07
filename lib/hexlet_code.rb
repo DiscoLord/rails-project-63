cba5eb35c84069bcb050d3de1447e1cf6e9d4918
Boris Nikolaev <boris28596@gmail.com>
2024-11-07 21:04:08 +0100

    Added project structure and first module with methods for tags

---
+++ lib/hexlet_code.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "hexlet_code/version"
+
+autoload :TagFactory, "hexlet_code/factory/tag_factory"
+
+module HexletCode
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hexlet_code/factory/tag_factory.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module TagFactory
+  def p(content)
+    "<p>#{content}</p>"
+  end
+  
+  def a(href, text)
+    "<a href='#{href}'>#{text}</a>"
+  end
+  
+  def img(src, alt_text)
+    "<img src='#{src}' alt='#{alt_text}'>"
+  end
+
+  def input(type, name, value = nil)
+    "<input type='#{type}' name='#{name}' value='#{value}'>"
+  end
+
+  def label(for_id, text)
+    "<label for='#{for_id}'>#{text}</label>"
+  end
+  
+  def button(text)
+    "<button>#{text}</button>"
+  end     
+end
