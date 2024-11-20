6ce6fd9aed4ed11eab68d6e7f384efb52c81f5e6
Boris Nikolaev <boris28596@gmail.com>
2024-11-20 14:52:55 +0100

    Changed autoloads

--- lib/hexlet_code/inputs.rb
+++ lib/hexlet_code/inputs.rb
@@ -3,7 +3,7 @@
 module HexletCode
   module Inputs
     autoload(:BaseInput, 'hexlet_code/inputs/base_input')
-    autoload(:StringInput, 'hexlet_code/inputs/string_input.rb')
-    autoload(:TextInput, 'hexlet_code/inputs/text_input.rb')
+    autoload(:StringInput, 'hexlet_code/inputs/string_input')
+    autoload(:TextInput, 'hexlet_code/inputs/text_input')
   end
 end
