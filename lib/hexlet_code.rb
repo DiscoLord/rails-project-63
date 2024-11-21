593060254b53afe92d2b65face69cd8d268586f9
Boris Nikolaev <boris28596@gmail.com>
2024-11-21 10:04:54 +0100

    Fixed autoload

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,13 +2,13 @@
 
 require_relative 'hexlet_code/version'
 
-require_relative 'hexlet_code/form_builder'
-require_relative 'hexlet_code/form_renderer'
-require_relative 'hexlet_code/tag'
-require_relative 'hexlet_code/inputs'
-
 # Main module
 module HexletCode
+  autoload :FormBuilder, 'hexlet_code/form_builder'
+  autoload :FormRenderer, 'hexlet_code/form_renderer'
+  autoload :Tag, 'hexlet_code/tag'
+  autoload :Inputs, 'hexlet_code/inputs'
+
   class Error < StandardError; end
 
   def self.form_for(entity, attributes = {})
