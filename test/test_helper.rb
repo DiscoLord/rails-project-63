4dbeeb333792e5d90536d7d36006c07f96807c86
Boris Nikolaev <boris28596@gmail.com>
2024-12-09 15:17:18 +0100

    removed commented out lines

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,5 @@
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'hexlet_code'
-# require 'hexlet_code/form_builder'
-# require 'hexlet_code/form_renderer'
-# require 'hexlet_code/tag'
-# require 'hexlet_code/inputs'
 
 require 'minitest/autorun'
